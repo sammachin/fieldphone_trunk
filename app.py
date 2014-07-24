db1594407f9d582131e6c634ff6676ffb48c07bb
Sam Machin <sam@sammachin.com>
2014-07-24 20:59:03 +0100

    1st

---
+++ app.py
@@ -0,0 +1,34 @@
+import cherrypy
+import os
+from cherrypy.process import servers
+from twilio import twiml
+import urllib
+
+
+
+def fake_wait_for_occupied_port(host, port): 
+	return
+
+servers.wait_for_occupied_port = fake_wait_for_occupied_port
+
+class Start(object):
+	def call(self, var=None, **params):
+		r = twiml.Response()
+		r.say("Welcome to field phone, please enter the number you wish to dial")
+		r.gather(numDigits=5, action="/input", method="POST")
+		return str(r)
+	def route(self, var=None, **params):
+		number = urllib.unquote(cherrypy.request.params['Digits'])
+		r = twiml.Response()
+		d = twiml.Dial()
+		d.sip("sip:%s@sip.com" % number)
+		r.append(d)
+		return str(r)
+	call.exposed = True
+	record.exposed = True
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0',})
+cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
+cherrypy.quickstart(Start())
+
+
