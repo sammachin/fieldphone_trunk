130538deea49cab3089454e8505dc43fb2d1a286
Sam Machin <sam@sammachin.com>
2014-07-24 21:06:22 +0100

    2

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ class Start(object):
 	def call(self, var=None, **params):
 		r = twiml.Response()
 		r.say("Welcome to field phone, please enter the number you wish to dial")
-		r.gather(numDigits=5, action="/input", method="POST")
+		r.gather(numDigits=5, action="/route", method="POST")
 		return str(r)
 	def route(self, var=None, **params):
 		number = urllib.unquote(cherrypy.request.params['Digits'])
 		r = twiml.Response()
 		d = twiml.Dial()
-		d.sip("sip:%s@sip.com" % number)
+		d.sip("sip:%s@phone.emf.camp:9252" % number)
 		r.append(d)
 		return str(r)
 	call.exposed = True
-	record.exposed = True
+	route.exposed = True
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0',})
 cherrypy.config.update({'server.socket_port': int(os.environ.get('PORT', '5000')),})
